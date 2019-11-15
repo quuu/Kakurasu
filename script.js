8a7726e72f455efced7f459c2a351231e64c40c5
Andrew Qu <qua@rpi.edu>
2019-11-15 12:52:40 -0500

    created initial looping through dom logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+function addHandlers(){
+
+    let grid = document.getElementsByClassName("grid")[0].children;
+    // let table = grid[2]
+   
+    let table = grid[1].children;
+    // console.log(table)
+    for (let i = 0; i < table.length; i++){
+        // console.log(table[i].children)
+        for (let j = 0; j < table[i].children.length; j++){
+            // console.log(table[i].children[i].innerHTML)
+            // console.log(table[i].children)
+        }
+    }
+    // console.log(grid[1].children)
+
+//   }
+}
+
+addHandlers()
