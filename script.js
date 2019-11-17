5a4be9aae083b7c25ba88f0c9f68429532b149e8
Andrew Qu <qua@rpi.edu>
2019-11-17 13:07:05 -0500

    started on actual game mechanism

--- script.js
+++ script.js
@@ -1,115 +1,115 @@
 
-function addHandlers(){
-
-
-    var table = document.getElementById("grid");
-
-    var cells = document.querySelectorAll("td");
-    console.log(cells)
-    for (var i = 0; i < cells.length; i++){
-        cells[i].addEventListener('click', function (e) {
-            let temp = e.currentTarget;
-            // if()
-            temp.style.backgroundColor = '#f00'
-        })
+// create underlying representation
+// to keep track of score
+var answer = new Array(6)
+for (let i = 0; i < answer.length; i++){
+    answer[i] = new Array(6);
+}
+for (let i = 0; i < answer.length; i++){
+    for (let j = 0; j < answer[i].length; j++){
+        answer[i][j] = Math.floor(Math.random() * 2)
     }
-// for (var i = 0, row; row = table.rows[i]; i++) {
-//         console.log("this is row " +row)
-//    //iterate through rows
-//    //rows would be accessed using the "row" variable assigned in the for loop
-//     for (var j = 0, col; col = row.cells[j]; j++) {
-//         console.log("this is col ")
-//         col.
-//         console.log(col)
-//      //iterate through columns
-//      //columns would be accessed using the "col" variable assigned in the for loop
-//    }  
-// }
-    // let grid = document.getElementsByClassName("grid")[0].children;
-    // // let table = grid[2]
-   
-    // let table = grid[1].children;
-    // // console.log(table)
-    // for (let i = 0; i < table.length; i++){
-    //     // console.log(table[i].children)
-    //     for (let j = 0; j < table[i].children.length; j++){
-    //         // console.log(table[i].children[i].innerHTML)
-    //         // console.log(table[i].children)
-    //     }
-    // }
-    // console.log(grid[1].children)
+}
 
-//   }
+var game_board = new Array(6)
+for (let i = 0; i < game_board.length; i++){
+    game_board[i] = new Array(6)
+}
+for (let i = 0; i < game_board.length; i++){
+    for (let j = 0; j < game_board[i].length; j++){
+        game_board[i][j] = 0
+    }
+}
+
+// variable to represent the values currently
+var up_to_down = new Array(6)
+
+var left_to_right = new Array(6)
+
+function check_win() {
+
 }
 
 var grid = clickableGrid(6,6,function(el,row,col,i){
+  /*
     console.log("You clicked on element:",el);
     console.log("You clicked on row:",row);
     console.log("You clicked on col:",col);
     console.log("You clicked on item #:",i);
 
-    // el.className = 'clicked';
-    
+*/
+
+
     if (el.className == 'clicked') {
-        el.className = ''
+      el.className = ''
+      game_board[row][col] = 0;
     }
     else {
-        el.className = 'clicked'
+      el.className = 'clicked'
+      game_board[row][col] = 1;
     }
+
+    console.log(game_board)
 });
 
 document.body.appendChild(grid);
 
-// console.log(grid)
-var table = document.getElementById("grid");
-for (var i = 0, row; row = table.rows[i]; i++) {
-    
-    // console.log(row)
-   //iterate through rows
-   //rows would be accessed using the "row" variable assigned in the for loop
-    for (var j = 0, col; col = row.cells[j]; j++) {
-        if (i == 0) {
-            // console.log("got")
-            var cloned = col.cloneNode(true);
-            col.parentNode.replaceChild(cloned, col)
-            console.log("got")
-        }
 
-        
-     //iterate through columns
-     //columns would be accessed using the "col" variable assigned in the for loop
-   }  
-}
-     
+
+
+
 function clickableGrid( rows, cols, callback ){
     var i = 0;
     var j = 0;
+
+    // create table
     var grid = document.createElement('table');
     grid.className = 'grid';
     grid.setAttribute("id", "grid")
+
+    // generate rows
     for (var r = 0; r < rows+1; ++r){
         var col = grid.appendChild(document.createElement('col'))
         col.style.width = "100";
         var tr = grid.appendChild(document.createElement('tr'));
+
+        // generate columns
         for (var c=0;c<cols+1;++c){
             var cell = tr.appendChild(document.createElement('td'));
             cell.style.height = "100";
-            if (r == 0 && i < rows) {
-                cell.innerHTML = i++;
-            }
-            if (c == 0 && j < cols) {
-                cell.innerHTML = j++;
-            }
-            else {
-                
+
+            // if it's either the top row, bottom row
+            // left column, right column, don't add click event to it
+
+            // main game pieces to click
+            if (r > 0 && r < rows && c > 0 && c < cols) {
+
                 cell.addEventListener('click',(function(el,r,c,i){
                     return function(){
                         callback(el,r,c,i);
                     }
                 })(cell,r,c,i),false);
             }
+            else {
+
+                if (r == 0 && i < rows) {
+                    cell.innerHTML = i++;
+                }
+                if (c == 0 && j < cols) {
+                    cell.innerHTML = j++;
+                }
+            }
         }
     }
+
+  for(let i=0;i<up_to_down.length;i++){
+
+
+  }
+  for(let i=0;i<left_to_write.length;i++){
+
+
+  }
     return grid;
 }
 // addHandlers()
